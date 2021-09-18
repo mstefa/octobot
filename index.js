9ffd0abd2d0850803ea5bfb2cd24272897afbf8c
mstefanutti <matias.stefanutti@mercadolibre.com>
2021-09-18 19:05:10 -0300

    initial commit: basic bot with basic commands

---
+++ index.js
@@ -0,0 +1,52 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+// config dot env
+dotenv.config();
+const token = process.env.TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+// Seting commands
+client.commands = new Collection();
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+// interactions
+client.once('ready', (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  }
+  catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
+});
+
+// Login to Discord with your client's token
+client.login(token);
